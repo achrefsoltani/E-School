4e15721f7f474104d033041215806cf38423cdfb
Achref Soltani <60237898+Ach-So@users.noreply.github.com>
2021-01-02 16:53:12 +0100

    crud seance added

---
+++ src/Form/SeanceType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Seance;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SeanceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('debut')
+            ->add('fin')
+            ->add('classe')
+            ->add('salle')
+            ->add('enseignant')
+            ->add('profs')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Seance::class,
+        ]);
+    }
+}
